1d5652d987caefdbd0ce8dab0f307c3f70543ce0
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-02-12 15:16:56 -0400

    fix: expose internal serial modules and upgrade version

--- src/lib.rs
+++ src/lib.rs
@@ -20,26 +20,36 @@ mod tooltip;
 
 mod theme;
 
-pub use application::*;
-pub use border::*;
-pub use button::*;
+pub use application::Application;
+pub use border::Border;
+pub use button::{Button, State as ButtonState};
 pub use color::Color;
-pub use container::*;
-pub use panegrid::*;
-pub use picklist::*;
-pub use progressbar::*;
-pub use scrollable::*;
-pub use textinput::*;
-pub use tooltip::*;
+pub use container::Container;
+pub use panegrid::{PaneGrid, State as PaneGridState};
+pub use picklist::{Menu as PicklistMenu, Picklist, State as PicklistState};
+pub use progressbar::ProgressBar;
+pub use scrollable::{Scrollable, State as ScrollableState};
+pub use textinput::{State as TextInputState, TextInput};
+pub use tooltip::Tooltip;
 
 pub use theme::Theme;
 
 pub mod serial {
+
     pub use crate::{
-        application::serial::*, border::serial::*, button::serial::*,
-        color::Color, container::serial::*, panegrid::serial::*,
-        picklist::serial::*, progressbar::serial::*,
-        scrollable::serial::*, textinput::serial::*, tooltip::serial::*,
+        application::serial::Application,
+        border::serial::Border,
+        button::serial::{Button, Component as ButtonComponent, State as ButtonState},
+        container::serial::Container,
+        panegrid::serial::{Component as PaneGridComponent, PaneGrid, State as PaneGridState},
+        picklist::serial::{
+            Menu as PicklistMenu, MenuComponent as PicklistMenuComponent, Picklist,
+            State as PicklistState, StateComponent as PicklistStateComponent,
+        },
+        progressbar::serial::ProgressBar,
+        scrollable::serial::{Scrollable, State as ScrollableState, Component as ScrollableComponent},
+        textinput::serial::{State as TextInputState, TextInput, Component as TextInputComponent},
+        tooltip::serial::Tooltip,
     };
 
     pub use crate::theme::serial::Theme;
